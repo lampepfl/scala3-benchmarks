3c1c1be6a515ea4c65b65359a3d6b08a01f9dad3
Matt Bovel <matthieu@bovel.net>
2026-02-04 08:26:44 +0100

    Use new nightly repo

--- scripts/lastNightly.scala
+++ scripts/lastNightly.scala
@@ -2,7 +2,7 @@
 //> using jvm temurin:25
 
 @main def run(): Unit =
-  val url = "https://repo1.maven.org/maven2/org/scala-lang/scala3-compiler_3/maven-metadata.xml"
+  val url = "https://repo.scala-lang.org/artifactory/maven-nightlies/org/scala-lang/scala3-compiler_3/maven-metadata.xml"
   val xml = scala.io.Source.fromURL(url).mkString
   val nightlyPattern = """<version>(.+-NIGHTLY)</version>""".r
   val nightlies = nightlyPattern.findAllMatchIn(xml).map(_.group(1)).toSeq
