257a1e434c8c41f61e4ac637bdf299258b79344b
Matt Bovel <matthieu@bovel.net>
2026-02-04 18:20:51 +0100

    Rename RuntimeBenchmarksNightly to RuntimeBenchmarksAwfyWeekly

--- bench/scala/RuntimeBenchmarksAwfyWeekly.scala
+++ bench/scala/RuntimeBenchmarksAwfyWeekly.scala
@@ -17,22 +17,7 @@ import permute.PermuteBenchmark
 import kmeans.KmeansBenchmark
 import org.scalajs.benchmark.tracer.Tracer
 
-class RuntimeBenchmarksNightly extends RuntimeBenchmarks:
-
-  @Benchmark def queens: Unit =
-    assert(QueensBenchmark.run())
-
-  @Benchmark def deltablue: Unit =
-    DeltaBlue.run()
-
-  @Benchmark def richards: Unit =
-    Richards.run()
-
-  @Benchmark def nbody: Unit =
-    assert(NbodyBenchmark.run(250000))
-
-  @Benchmark def mandelbrot: Unit =
-    assert(MandelbrotBenchmark.run(750) == 50)
+class RuntimeBenchmarksAwfyWeekly extends RuntimeBenchmarks:
 
   @Benchmark def bounce: Unit =
     assert(BounceBenchmark.run(100000) == 1278008)
@@ -43,20 +28,35 @@ class RuntimeBenchmarksNightly extends RuntimeBenchmarks:
   @Benchmark def cd: Unit =
     assert(CDBenchmark.run(100) == 4305)
 
+  @Benchmark def deltablue: Unit =
+    DeltaBlue.run()
+
   @Benchmark def gcbench: Unit =
     assert(GCBenchBenchmark.run())
 
   @Benchmark def json: Unit =
     assert(JsonBenchmark.run(JsonBenchmark.input) == 156)
 
+  @Benchmark def kmeans: Unit =
+    assert(KmeansBenchmark.run(100000))
+
   @Benchmark def list: Unit =
     assert(ListBenchmark.run(9) == 18)
+
+  @Benchmark def mandelbrot: Unit =
+    assert(MandelbrotBenchmark.run(750) == 50)
+
+  @Benchmark def nbody: Unit =
+    assert(NbodyBenchmark.run(250000))
+
+  @Benchmark def queens: Unit =
+    assert(QueensBenchmark.run())
 
   @Benchmark def permute: Unit =
     assert(PermuteBenchmark.run(10) == 3628800)
 
-  @Benchmark def kmeans: Unit =
-    assert(KmeansBenchmark.run(100000))
+  @Benchmark def richards: Unit =
+    Richards.run()
 
   @Benchmark def tracer: Unit =
     Tracer.run()
