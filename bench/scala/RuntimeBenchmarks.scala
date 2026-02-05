7126925479cd3c5619031ffff7f5d1bdc99d0583
Matt Bovel <matthieu@bovel.net>
2026-02-05 18:41:44 +0100

    Fine-tune batch sizes for Awfy benchmark
    So that each benchmark takes tens of milliseconds at least

--- bench/scala/RuntimeBenchmarks.scala
+++ bench/scala/RuntimeBenchmarks.scala
@@ -5,8 +5,8 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 import org.openjdk.jmh.annotations.{BenchmarkMode, Fork, Measurement, OutputTimeUnit, Scope, State, Warmup}
 
 @Fork(value = 1, jvmArgsPrepend = Array("-Xms2G", "-Xmx2G"))
-@Warmup(iterations = 60)
-@Measurement(iterations = 20)
+@Warmup(iterations = 20)
+@Measurement(iterations = 10)
 @BenchmarkMode(Array(org.openjdk.jmh.annotations.Mode.SingleShotTime))
 @State(Scope.Benchmark)
 @OutputTimeUnit(MILLISECONDS)
--- bench/scala/RuntimeBenchmarksAwfyWeekly.scala
+++ bench/scala/RuntimeBenchmarksAwfyWeekly.scala
@@ -1,6 +1,6 @@
 package bench
 
-import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.{Benchmark, Measurement, Warmup}
 
 import bounce.BounceBenchmark
 import brainfuck.BrainfuckBenchmark
@@ -19,44 +19,78 @@ import tracer.Tracer
 
 class RuntimeBenchmarksAwfyWeekly extends RuntimeBenchmarks:
 
-  @Benchmark def bounce: Unit =
+  @Warmup(batchSize = 1000, iterations = 10)
+  @Measurement(batchSize = 1000, iterations = 10)
+  @Benchmark
+  def bounce: Unit =
     assert(BounceBenchmark.run(100) == 1331)
 
-  @Benchmark def brainfuck: Unit =
+  @Warmup(batchSize = 10, iterations = 10)
+  @Measurement(batchSize = 10, iterations = 10)
+  @Benchmark
+  def brainfuck: Unit =
     assert(BrainfuckBenchmark.run() == 11359)
 
-  @Benchmark def cd: Unit =
+  @Benchmark
+  def cd: Unit =
     assert(CDBenchmark.run(100) == 4305)
 
-  @Benchmark def deltablue: Unit =
+  @Warmup(batchSize = 100, iterations = 10)
+  @Measurement(batchSize = 100, iterations = 10)
+  @Benchmark
+  def deltablue: Unit =
     DeltaBlue.run()
 
-  @Benchmark def gcbench: Unit =
+  @Benchmark
+  def gcbench: Unit =
     assert(GCBenchBenchmark.run())
 
-  @Benchmark def json: Unit =
+  @Warmup(batchSize = 10, iterations = 10)
+  @Measurement(batchSize = 10, iterations = 10)
+  @Benchmark
+  def json: Unit =
     assert(JsonBenchmark.run(JsonBenchmark.input) == 156)
 
-  @Benchmark def kmeans: Unit =
+  @Benchmark
+  def kmeans: Unit =
     assert(KmeansBenchmark.run(100000))
 
-  @Benchmark def list: Unit =
+  @Warmup(batchSize = 1000, iterations = 10)
+  @Measurement(batchSize = 1000, iterations = 10)
+  @Benchmark
+  def list: Unit =
     assert(ListBenchmark.run(5) == 10)
 
-  @Benchmark def mandelbrot: Unit =
+  @Benchmark
+  def mandelbrot: Unit =
     assert(MandelbrotBenchmark.run(750) == 50)
 
-  @Benchmark def nbody: Unit =
+  @Warmup(batchSize = 10, iterations = 10)
+  @Measurement(batchSize = 10, iterations = 10)
+  @Benchmark
+  def nbody: Unit =
     assert(NbodyBenchmark.run(250000))
 
-  @Benchmark def queens: Unit =
+  @Warmup(batchSize = 10000, iterations = 10)
+  @Measurement(batchSize = 10000, iterations = 10)
+  @Benchmark
+  def queens: Unit =
     assert(QueensBenchmark.run())
 
-  @Benchmark def permute: Unit =
+  @Warmup(batchSize = 1000, iterations = 10)
+  @Measurement(batchSize = 1000, iterations = 10)
+  @Benchmark
+  def permute: Unit =
     assert(PermuteBenchmark.run(6) == 720)
 
-  @Benchmark def richards: Unit =
+  @Warmup(batchSize = 1000, iterations = 10)
+  @Measurement(batchSize = 1000, iterations = 10)
+  @Benchmark
+  def richards: Unit =
     Richards.run()
 
-  @Benchmark def tracer: Unit =
+  @Warmup(batchSize = 100, iterations = 10)
+  @Measurement(batchSize = 100, iterations = 10)
+  @Benchmark
+  def tracer: Unit =
     Tracer.run()
