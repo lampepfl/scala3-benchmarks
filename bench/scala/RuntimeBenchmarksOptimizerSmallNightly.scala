a121d3fd03b505b29c34fa3c30cbfea21870844f
Solal Pirelli <solal.pirelli@gmail.com>
2026-02-06 12:35:09 +0100

    oops

--- bench/scala/RuntimeBenchmarksOptimizerSmallNightly.scala
+++ bench/scala/RuntimeBenchmarksOptimizerSmallNightly.scala
@@ -20,7 +20,7 @@ class RuntimeBenchmarksOptimizerSmallNightly extends RuntimeBenchmarks:
 
   @Benchmark
   def meteor: Unit =
-    assert(Meteor.main() == "9 9 9 9 8 \n 9 6 6 8 5 \n6 6 8 8 5 \n 6 8 2 5 5 \n7 7 7 2 5 \n 7 4 7 2 0 \n1 4 2 2 0 \n 1 4 4 0 3 \n1 4 0 0 3 \n 1 1 3 3 3 ")
+    assert(Meteor.main() == "9 9 9 9 8 \n 9 6 6 8 5 \n6 6 8 8 5 \n 6 8 2 5 5 \n7 7 7 2 5 \n 7 4 7 2 0 \n1 4 2 2 0 \n 1 4 4 0 3 \n1 4 0 0 3 \n 1 1 3 3 3 \n")
 
   @Benchmark
   def nestedLoop: Unit =
